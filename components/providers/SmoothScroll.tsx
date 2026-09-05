cd1c91a02a342210cafdfa7e9c5caa30ca5bb304
Kas Denge Engineering <engineering@kasdenge.com>
2026-09-05 14:06:34 +0530

    fix(deps): replace deprecated @studio-freight/react-lenis with lenis/react and add .npmrc for React 19 Vercel deployment

--- components/providers/SmoothScroll.tsx
+++ components/providers/SmoothScroll.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ReactLenis, useLenis } from "@studio-freight/react-lenis";
+import { ReactLenis, useLenis } from "lenis/react";
 import { usePathname, useSearchParams } from "next/navigation";
 import { useEffect, Suspense } from "react";
 import gsap from "gsap";
